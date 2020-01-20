ac005a53e73224c49f3e9d53a4ed90c246576bfe
FynnTheuser <59827472+FynnTheuser@users.noreply.github.com>
2020-01-20 09:27:38 +0100

    Add files via upload

---
+++ 4gewinnt/src/model/HelloWorld.java
@@ -0,0 +1,103 @@
+
+public class HelloWorld {
+
+	private static final int Berlin = 0;
+	private static int Wildeshausen;
+
+	public static void main(String[] args) {
+
+		// int alter;
+		// alter = 16;
+		int ergebnis;
+
+		// System.out.println(alter);
+
+		int gewicht = 80;
+
+		// System.out.println(gewicht);
+		// ergebnis = alter + gewicht;
+
+		// System.out.println("Die Summe von dem Alter und dem Gewicht ist = ");
+		// System.out.println(ergebnis);
+
+		/*
+		 * int note1 = 8; int note2 = 8; int note3 = 7; int note4 = 7; int note5 = 9;
+		 * int note6 = 10; int note7 = 12; int note8 = 6; int note9 = 9; int note10 =
+		 * 14; int note11 = 6; int note12 = 7; int note13 = 8;
+		 */
+
+		// System.out.println((note1 + note2 + note3 + note4 + note5 + note6 + note7 +
+		// note8 + note9 + note10 + note11 + note12 + note13) / 13);
+
+		/*
+		 * int a = 10;
+		 * 
+		 * int b = 0;
+		 * 
+		 * do { System.out.println("Hello");
+		 * 
+		 * b++; }
+		 * 
+		 * while (a>b); System.out.println("Bye");
+		 */
+
+		/*
+		 * int a = 10; int b = 11; if (a > b) {
+		 * System.out.println("A ist größer als B"); }
+		 * 
+		 * else if (b > a) { System.out.println("B ist größer als A"); } else {
+		 * System.out.println("A ist gleich groß wie B"); }
+		 * 
+		 * int alter = 16;
+		 * 
+		 * switch (alter) {
+		 * 
+		 * case 12: System.out.println("Du bist 12 Jahre alt!"); break; case 13:
+		 * System.out.println("Du bist 13 Jahre alt!"); break; case 14:
+		 * System.out.println("Du bist 14 Jahre alt!"); break; case 15:
+		 * System.out.println("Du bist 15 Jahre alt!"); break; case 16:
+		 * System.out.println("Du bist 16 Jahre alt!"); break; case 17:
+		 * System.out.println("Du bist 17 Jahre alt!"); break;
+		 */
+
+		/*
+		 * int läuft = 10;
+		 * 
+		 * for(int i = 0; i<läuft; i++) {
+		 * 
+		 * System.out.print("Runde: "+i); }
+		 * 
+		 * 
+		 * 
+		 * for(int i = 1; i<=5; i++) { for (int j=1;j<=i;j++) {
+		 * 
+		 * System.out.print("*"); } System.out.println();
+		 */
+
+		int[] array = {  1, 2, 3, 4, 5, 6 };
+		int[] array2 = { 7, 8, 9 };
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
+		for (int i = 0; i < array2.length; i++) {
+			System.out.println(array2[i]);
+				a
+		}
+	}
+
+	// String[][] zDA = {{"1","2"},{"3","4"},{"5","6"}};
+
+	// System.out.println(zDA[1][0]);
+	// System.out.println(zDA[2][1]);
+
+	/*
+	 * int apfel = 4; int banane = 4;
+	 * 
+	 * if (apfel>banane) { System.out.println("Der Apfel ist teurer"); } else if
+	 * (banane>apfel) { System.out.println("Die Banane ist teurer"); } else {
+	 * System.out.println("Beides ist gleich teuer"); }
+	 */
+
+}
