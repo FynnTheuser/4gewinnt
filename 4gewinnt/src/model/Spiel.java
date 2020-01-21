07386fe4bb9a3f27fa5628dbe0f6a39daaf578d9
FynnTheuser <59827472+FynnTheuser@users.noreply.github.com>
2020-01-21 14:34:39 +0100

    Add files via upload

--- 4gewinnt/src/model/Spiel.java
+++ 4gewinnt/src/model/Spiel.java
@@ -29,9 +29,100 @@ public class Spiel {
 public static void printSpielfeld(Spielstein[][] spielbrett) {
 		for (Spielstein[] x : spielbrett) {
 			for (Spielstein spielstein : x) {
-				/*1.if benutzen, um zu schauen, ob dort ein Spielstein ist und welche Farbe dieser hat 2. array hübsch formatieren 3. durch scannen lassen
+				if (spielbrett == ' ') }
+				else (spielbrett == Color.GREEN || Color.RED)
+					if(Color.GREEN == spielstein)
+						Spielstein = spielsteinGrün;
+					if(Color.RED == spielstein)
+						spielbrett = spielsteinRot;
+					
+				}
+
+	public static int gewonnenVertikal(char spielbrett[][], char spielstein, String name, int i) {
+
+		for (int z = 5; z >= 3; z--) {
+			for (int s = 0; s < spielbrett[z].length; s++) {
+				if (spielbrett[z][s] == spielstein && spielbrett[z - 1][s] == spielstein
+						&& spielbrett[z - 2][s] == spielstein && spielbrett[z - 3][s] == spielstein) {
+					System.out.println(name + " hat gewonnen!");
+
+				}
 			}
 		}
+
+	public static int gewonnenWaagerecht(char spielbrett[][],
+				char spielstein, String name, int i) {
+
+			for (int z = 0; z < spielbrett.length; z++) 
+			{
+				for (int s = 0; s <= 3; s++) 
+				{
+					if (spielbrett[z][s] == spielstein
+							&& spielbrett[z][s + 1] == spielstein
+							&& spielbrett[z][s + 2] == spielstein
+							&& spielbrett[z][s + 3] == spielstein)
+					{
+						System.out.println("Sieger" + name);
+					
+					}
+				}
+			}
+
+	public static int gewonnenDiagonal1(char spielbrett[][],
+					char spielstein, String name, int i) {
+
+				for (int z = 0; z <= 2; z++) 
+				{
+					for (int s = 0; s <= 3; s++)
+					{
+					if (spielbrett[z][s] == spielstein
+							&& spielbrett[z + 1][s + 1] == spielstein
+							&& spielbrett[z + 2][s + 2] == spielstein
+							&& spielbrett[z + 3][s + 3] == spielstein)
+
+						System.out.println("Sieger" + name);
+					}
+				}
+	
 	}
+
+	public static int gewonnenDiagonal2(char spielbrett[][],
+						char spielstein, String name, int i) {
+
+					for (int z = 0; z <= 2; z++) 
+					{
+						for (int s = 6; s >= 3; s--) 
+							if (spielbrett[z][s] == spielstein
+									&& spielbrett[z + 1][s - 1] == spielstein
+									&& spielbrett[z + 2][s - 2] == spielstein
+									&& spielbrett[z + 3][s - 3] == spielstein) {
+								System.out.println( "Sieger" + name);	
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+	/*
+	 * 1.if benutzen, um zu schauen, ob dort ein Spielstein ist und welche Farbe
+	 * dieser hat 2. array hübsch formatieren 3. durch scannen lassen
+	 */
 
 }
